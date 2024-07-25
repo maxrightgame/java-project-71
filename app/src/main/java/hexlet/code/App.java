27cf95f12a31400a7ae40da056e4ad5d3b831be5
Max <maxrightgame@gmail.com>
2024-07-25 11:30:28 +0600

    Complete step 4 (fixed -h output)

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,20 +4,44 @@ import picocli.CommandLine;
 
 import java.util.concurrent.Callable;
 
-@CommandLine.Command(name = "getDiff", mixinStandardHelpOptions = true, version = "getDiff 1.0",
-                     description = "Compares two configuration files and shows a difference.")
+@CommandLine.Command(
+                    name = "getDiff",
+                    mixinStandardHelpOptions = true,
+                    version = "getDiff 1.0",
+                    description = "Compares two configuration files and shows a difference."
+)
 
 public class App implements Callable<Integer> {
 
-    @CommandLine.Option(names = {"-fh", "--filePathFirst"}, description = "path to first file")
+    @CommandLine.Parameters(
+                        index = "0",
+                        paramLabel = "filePathFirst",
+                        description = "path to first file"
+    )
     private String filePathFirst;
-    @CommandLine.Option(names = {"-fs", "--filePathSecond"}, description = "path to second file")
+    @CommandLine.Parameters(
+                        index = "1",
+                        paramLabel = "filePathSecond",
+                        description = "path to second file"
+    )
     private String filePathSecond;
-    @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    @CommandLine.Option(
+                        names = {"-h", "--help"},
+                        usageHelp = true,
+                        description = "Show this help message and exit."
+    )
     boolean usageHelpRequested;
-    @CommandLine.Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    @CommandLine.Option(
+                        names = {"-V", "--version"},
+                        versionHelp = true,
+                        description = "Print version information and exit."
+    )
     boolean versionInfoRequested;
-    @CommandLine.Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    @CommandLine.Option(
+                        names = {"-f", "--format"},
+                        paramLabel = "format",
+                        description = "output format [default: stylish]"
+    )
     private String format = "stylish";
 
     @Override
