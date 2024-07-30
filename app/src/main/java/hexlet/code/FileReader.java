5f8131dfef20e028e3e0c4a229afb4cb83fe8153
Max <maxrightgame@gmail.com>
2024-07-30 16:28:54 +0600

    Fix FileReader.java (add absolute pathing)

--- app/src/main/java/hexlet/code/FileReader.java
+++ app/src/main/java/hexlet/code/FileReader.java
@@ -2,10 +2,11 @@ package hexlet.code;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileReader {
     public static String readFile(String filePath) throws Exception {
-        Path path = Path.of(filePath);
+        Path path = Paths.get(filePath).toAbsolutePath().normalize();
         return Files.readString(path);
     }
 }
